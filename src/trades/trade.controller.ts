5249035a63b739966b612a30d1ef94c672687b5b
Vova Rudakov <vova.rudakov@gmail.com>
2025-02-20 14:52:51 +0000

    service to analyze historic data

--- src/trades/trade.controller.ts
+++ src/trades/trade.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { TradeService } from './trade.service';
-import { DateRangeDto } from './dto/date-range.dto';
+import { AnalyzeTimeRangeDto } from './dto/analyze-time-range.dto';
 
 @Controller('trades')
 export class TradeController {
   constructor(private readonly tradeService: TradeService) {}
 
   @Post('analyze')
-  async analyze(@Body() dateRangeDto: DateRangeDto) {
-
+  async analyze(@Body() analyzeTimeRangeDto: AnalyzeTimeRangeDto) {
+    return this.tradeService.analyzeHistoricTrades(
+      analyzeTimeRangeDto.symbol,
+      analyzeTimeRangeDto.startTime,
+      analyzeTimeRangeDto.endTime,
+    );
   }
 }
--- src/trades/trade.service.ts
+++ src/trades/trade.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { BinanceClient } from '../binance-client/binance.client';
+import * as _ from 'lodash';
+import { TradeDto } from '../binance-client/dto/trade.dto';
 
 @Injectable()
-export class TradeService {}
+export class TradeService {
+  constructor(private readonly binanceClient: BinanceClient) {}
+
+  public async analyzeHistoricTrades(
+    symbol: string,
+    startTime: number,
+    endTime: number,
+  ) {
+    let historicalTrades: TradeDto[];
+
+    try {
+      historicalTrades = await this.binanceClient.getHistoricalTrades(
+        symbol,
+        startTime,
+        endTime,
+      );
+    } catch (e) {
+      throw new HttpException('Could not retrieve historic trades', e);
+    }
+
+    const tradeWithLowestPrice = _.minBy(historicalTrades, 'price');
+    const tradeWithHighestPrice = _.maxBy(historicalTrades, 'price');
+    let priceIncreasedCounter = 0;
+    let priceDecreasedCounter = 0;
+    let maxIncreasedBy = 0;
+    let maxDecreasedBy = 0;
+
+    for (let i = 0; i < historicalTrades.length - 1; i++) {
+      const change =
+        Number(historicalTrades[i + 1].price) -
+        Number(historicalTrades[i].price);
+
+      if (change > 0) {
+        priceIncreasedCounter++;
+        if (maxIncreasedBy < change) {
+          maxIncreasedBy = change;
+        }
+      } else if (change < 0) {
+        priceDecreasedCounter++;
+        if (maxDecreasedBy < Math.abs(change)) {
+          maxDecreasedBy = Math.abs(change);
+        }
+      }
+    }
+
+    return {
+      totalTrades: historicalTrades.length,
+      priceDecreasedTotalTimes: priceDecreasedCounter,
+      priceIncreasedTotalTimes: priceIncreasedCounter,
+      maxIncreasedBy,
+      maxDecreasedBy,
+      tradeWithLowestPrice,
+      tradeWithHighestPrice,
+    };
+  }
+}
